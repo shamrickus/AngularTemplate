207a91754f91c9197e6074d4677ef0de6d4d691c
shamrickus <shamrickus@gmail.com>
2019-05-12 12:03:34 -0600

    Begone

--- karma.conf.js
+++ karma.conf.js
@@ -10,16 +10,11 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-teamcity-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
