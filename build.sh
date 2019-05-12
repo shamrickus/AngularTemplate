e77b3b2704d930cfa2695ba0e52802773077eeae
shamrickus <shamrickus@gmail.com>
2019-05-12 13:33:31 -0600

    Fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ./node_modules/.bin/npm run build
+sudo npm run build
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ./node_modules/.bin/npm i --save-dev --no-optional
+sudo npm i --save-dev --no-optional
